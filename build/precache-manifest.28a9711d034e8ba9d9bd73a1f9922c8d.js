7d4df8ce0f9764ff88c69012ee9821d3b9e00758
Benison <sachinlg60@gmail.com>
2020-07-12 13:03:33 +0530

    Enabled SSR

---
+++ build/precache-manifest.28a9711d034e8ba9d9bd73a1f9922c8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea32dcba48299d97035834181470a68f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12ccb2b4ed93c7285acd",
+    "url": "/static/css/main.a2ee701e.chunk.css"
+  },
+  {
+    "revision": "a30aba639b206ae0154c",
+    "url": "/static/js/2.64e905fc.chunk.js"
+  },
+  {
+    "revision": "12ccb2b4ed93c7285acd",
+    "url": "/static/js/main.161d3980.chunk.js"
+  },
+  {
+    "revision": "17848ce741cf0288e512",
+    "url": "/static/js/runtime-main.dfac7ffd.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import HackerNews from './HackerNews';
+import React from "react";
+import "./App.css";
+import HackerNews from "./HackerNews";
+
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <HackerNews />
+   <HackerNews />
   );
 }
 
